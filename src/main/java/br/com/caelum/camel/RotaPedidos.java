794415b1ca34d80ee81e436a59de79a0c33eff4d
marcelo.sales <marcelo.sales@cqgmtz-497-0960.queirozgalvao.com>
2017-02-20 17:01:03 -0300

    Aula 5

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -18,24 +18,25 @@ public class RotaPedidos {
 			@Override
 			public void configure() throws Exception {
 				from("file:pedidos?delay=5s&noop=true").multicast().to("direct:soap").to("direct:http");
-				
-				from("direct:http").routeId("rota-http")
-						.setProperty("pedidoId", xpath("/pedido/id/text()"))
-						.setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()"))
-						.split().xpath("/pedido/itens/item")
-						.filter().xpath("/item/formato[text()='EBOOK']")
-						.setProperty("ebookId", xpath("/item/livro/codigo/text()"))
-						.marshal().xmljson()						
+
+				from("direct:http").routeId("rota-http").setProperty("pedidoId", xpath("/pedido/id/text()"))
+						.setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).split()
+						.xpath("/pedido/itens/item").filter().xpath("/item/formato[text()='EBOOK']")
+						.setProperty("ebookId", xpath("/item/livro/codigo/text()")).marshal().xmljson()
 						.setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET))
-						.setHeader(Exchange.HTTP_QUERY,	simple("clienteId=${exchangeProperty.clienteId}&pedidoId=${exchangeProperty.pedidoId}&ebookId=${exchangeProperty.ebookId}"))
-				.to("http4://localhost:8080/webservices/ebook/item");
-				
-				from("direct:soap").routeId("rota-soap").log("chamando serviço soap ${body}").to("mock:soap");
+						.setHeader(Exchange.HTTP_QUERY,
+								simple("clienteId=${exchangeProperty.clienteId}&pedidoId=${exchangeProperty.pedidoId}&ebookId=${exchangeProperty.ebookId}"))
+						.to("http4://localhost:8080/webservices/ebook/item");
+
+				from("direct:soap").routeId("rota-soap").to("xslt:pedido-para-soap.xslt")
+						.log("Resultado do Template: ${body}").setHeader(Exchange.CONTENT_TYPE, constant("text/xml"))
+						.to("http4://localhost:8080/webservices/financeiro");
 
 			}
 
 		});
 		context.start();
-		TimeUnit.SECONDS.sleep(20);
+		TimeUnit.SECONDS.sleep(10);
+		context.stop();
 	}
 }
