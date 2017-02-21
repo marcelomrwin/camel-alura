bbc28954d267116b7f544a8316b9fec76bc522f4
Marcelo Daniel <marcelo.d.sales@gmail.com>
2017-02-21 07:12:07 -0300

    error handle

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -4,9 +4,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.xml.sax.SAXParseException;
 
 public class RotaPedidos {
 
@@ -17,20 +19,39 @@ public class RotaPedidos {
 
 			@Override
 			public void configure() throws Exception {
-				from("file:pedidos?delay=5s&noop=true").multicast().to("direct:soap").to("direct:http");
+
+								errorHandler(deadLetterChannel("file:erro").maximumRedeliveries(3).redeliverDelay(3000).onRedelivery(new Processor() {
+									@Override
+									public void process(Exchange exchange) throws Exception {
+										int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+				                        int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+				                        System.out.println("Redelivery - " + counter + "/" + max );			
+									}
+								}));
+
+//				onException(SAXParseException.class).handled(true).maximumRedeliveries(3).redeliverDelay(4000).onRedelivery(new Processor() {
+//
+//					@Override
+//					public void process(Exchange exchange) throws Exception {
+//						int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+//						int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+//						System.out.println("Redelivery - " + counter + "/" + max);
+//					}
+//				});
+
+				from("file:pedidos?delay=5s&noop=true").routeId("rota-pedidos").to("validator:pedido.xsd").multicast().to("direct:soap")
+						.to("direct:http");
 
 				from("direct:http").routeId("rota-http").setProperty("pedidoId", xpath("/pedido/id/text()"))
-						.setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).split()
-						.xpath("/pedido/itens/item").filter().xpath("/item/formato[text()='EBOOK']")
-						.setProperty("ebookId", xpath("/item/livro/codigo/text()")).marshal().xmljson()
-						.setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET))
+						.setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).split().xpath("/pedido/itens/item")
+						.filter().xpath("/item/formato[text()='EBOOK']").setProperty("ebookId", xpath("/item/livro/codigo/text()"))
+						.marshal().xmljson().setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.GET))
 						.setHeader(Exchange.HTTP_QUERY,
 								simple("clienteId=${exchangeProperty.clienteId}&pedidoId=${exchangeProperty.pedidoId}&ebookId=${exchangeProperty.ebookId}"))
 						.to("http4://localhost:8080/webservices/ebook/item");
 
-				from("direct:soap").routeId("rota-soap").to("xslt:pedido-para-soap.xslt")
-						.log("Resultado do Template: ${body}").setHeader(Exchange.CONTENT_TYPE, constant("text/xml"))
-						.to("http4://localhost:8080/webservices/financeiro");
+				from("direct:soap").routeId("rota-soap").to("xslt:pedido-para-soap.xslt").log("Resultado do Template: ${body}")
+						.setHeader(Exchange.CONTENT_TYPE, constant("text/xml")).to("http4://localhost:8080/webservices/financeiro");
 
 			}
 
